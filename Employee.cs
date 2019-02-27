17ff1eae33bc23e0009e3f050aa484f8bb3448a5
Dima Naumow <dimanaumow123@gmail.com>
2019-02-27 18:48:37 +0300

    first attempt

---
+++ Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string surname;
+
+        private int age;
+
+        public Employee()
+        {
+                    
+        }
+
+        public Employee(string surname, int age)
+        { 
+            this.age = age;
+
+            this.SetSurname(surname); 
+        }
+
+        public void SetSurname(string surname)
+        {
+            if(surname == null)
+            {
+                throw new ArgumentNullException(); 
+            }
+
+            this.surname = surname; 
+        }
+
+        private string GetAge() => this.age.ToString(); 
+
+        public string Info()
+        {
+            return "Surname: " + this.surname + ", Age: " + this.age; 
+        }
+    }
+}
